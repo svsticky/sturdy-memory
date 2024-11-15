7fbe863729a1b4b3e6195900c410af9f82deaf96
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-11-15 17:41:31 +0100

    feat: /product/status

---
+++ server/src/web/routes/product/status.py
@@ -0,0 +1,33 @@
+from flask import request
+from werkzeug.exceptions import BadRequest
+
+from server.src.database.setup import db, conn
+from server.src.web.utils import filter_name
+
+
+def status_get():
+    data = request.json
+    filtered_name = filter_name(data["name"])
+
+    db.execute(f"SELECT * FROM current_stock WHERE name LIKE '{filtered_name}' ")
+
+    query = db.fetchall()
+
+    if len(query) == 0:
+        raise BadRequest(description=f"Product {filtered_name} doesn't exist")
+
+    return {"quantity": query[0][2]}, 200
+
+
+def status_post():
+    data = request.json
+    filtered_name = filter_name(data["name"])
+
+    # Enforce that the quantity is a positive integer
+    if not isinstance(data["quantity"], int) or data["quantity"] < 0:
+        raise BadRequest(description="Invalid quantity")
+
+    db.execute(f"UPDATE current_stock SET quantity = {data["quantity"]} WHERE name LIKE '{filtered_name}';")
+    conn.commit()
+
+    return "200 OK\n"
