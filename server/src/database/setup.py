da8e536f0a6cfd0ed6a3c576b84f190868d5cc1a
sam32123 <samstaijen@gmail.com>
2024-11-25 17:41:10 +0100

    updated CREATE TABLE to comply with satex idea. Webapp should be modified to support this

--- server/src/database/setup.py
+++ server/src/database/setup.py
@@ -14,17 +14,33 @@ db = conn.cursor()
 def setup_table():
     try:
         db.execute("""
-create table current_stock
-(
-    id       bigserial
-        constraint primary_key
-            primary key,
-    name     varchar(64)       not null,
-    quantity integer default 0 not null
+CREATE TABLE Producten (
+    Product_ID SERIAL PRIMARY KEY,
+    Product_Naam VARCHAR(255) NOT NULL,
+    Product_Aantal INT NOT NULL,
+    Product_Barcode VARCHAR(255)
 );
 
-alter table current_stock
-    owner to dev;
+CREATE TABLE Transacties (
+    Transactie_ID SERIAL PRIMARY KEY,
+    Product_ID INT NOT NULL,
+    Date_Time TIMESTAMP NOT NULL,
+    Transactie_Aantal INT NOT NULL,
+    FOREIGN KEY (Product_ID) REFERENCES Producten(Product_ID)
+);
+
+CREATE TABLE Derving (
+    Transactie_ID INT NOT NULL,
+    User_ID INT NOT NULL,
+    Derving_Type VARCHAR(255),
+    PRIMARY KEY (Transactie_ID, User_ID),
+    FOREIGN KEY (Transactie_ID) REFERENCES Transacties(Transactie_ID)
+);
+
+CREATE TABLE Succesvolle_Transacties (
+    Transactie_ID INT PRIMARY KEY,
+    FOREIGN KEY (Transactie_ID) REFERENCES Transacties(Transactie_ID)
+);
         """)
     except psycopg2.errors.DuplicateTable:
         print("Table already exists.")
